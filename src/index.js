73ef685120c2d7c8ae02445c06ccacc04df52774
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-11-17 10:55:14 -0800

    fix app, index, package.json, and add yarn.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@chakra-ui/core';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
