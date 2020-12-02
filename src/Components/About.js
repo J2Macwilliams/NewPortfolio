3b6a93eaea06695f7cfdca421e461a93947a3fbe
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-12-01 16:29:29 -0800

    clean up about

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,17 +5,9 @@ import "./Style/About.css"
 const About = () => {
   return (
     <div id="about">
-
-      <div className="greet">
-        What's his story?
-
-      </div>
+      <h1>What's his story?</h1>
       <div className="storyBox">
-        {/* <div className="photoBox"> */}
-          <img className="myPhoto" src={ Jeremy } alt="Jeremy" />
-        {/* </div> */}
-
-
+        <img className="myPhoto" src={ Jeremy } alt="Jeremy" />
         <p className="blurb"> I'm a Full-Stack Software Engineer with a passion for creativity, communication, and collaboration.<br /><br />
         Born and raised in Kansas, I seized the opportunity to escape, following theatrical passions and the hope for an exciting adventure. Along the journey, I discovered that to reach the metaphorical "Emerald City in the Land of OZ" I would have to become the wizard behind the curtain.<br /><br />
         Cue heroic montage of life lesson building experiences resulting in a multi-skilled problem solver and teammate. Through these labors, I discovered programming. Initially I taught myself the basics of HTML, CSS, and JS and later attended Lambda School learning React, Redux, and Node. I'm currently learning GraphQL, AWS, and Docker.<br /><br />
